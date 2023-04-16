1656b5628a21a13279414060c14de1bc994eae79
Rukia0124 <asahari02@gmail.com>
2023-04-17 00:40:22 +0200

    fisrt commit

---
+++ backend/routes/book.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const bookCtrl = require("../controllers/book");
+const auth = require("../middleware/auth");
+const multer = require("../middleware/multer-config");
+
+router.get("/", bookCtrl.getAllBooks);
+router.post("/", auth, multer, bookCtrl.createBook);
+router.get("/bestrating", bookCtrl.bestRating);
+router.get("/:id", bookCtrl.getOneBook);
+router.delete("/:id", auth, bookCtrl.deleteBook);
+router.put("/:id", auth, multer, bookCtrl.modifyBook);
+router.post("/:id/rating", auth, bookCtrl.addBookRating);
+
+module.exports = router;
