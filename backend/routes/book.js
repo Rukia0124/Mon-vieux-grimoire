80e7dfcf10900175dcaea3e8042dde409abcb649
Rukia0124 <asahari02@gmail.com>
2023-04-27 21:25:46 +0200

    refactor: added dot env fort port on server

--- backend/routes/book.js
+++ backend/routes/book.js
@@ -3,13 +3,14 @@ const router = express.Router();
 const bookCtrl = require("../controllers/book");
 const auth = require("../middlewares/auth");
 const multer = require("../middlewares/multer-config");
+const imageCompression = require("../middlewares/imageCompression");
 
 router.get("/", bookCtrl.getAllBooks);
-router.post("/", auth, multer, bookCtrl.createBook);
+router.post("/", auth, multer, imageCompression, bookCtrl.createBook);
 router.get("/bestrating", bookCtrl.bestRating);
 router.get("/:id", bookCtrl.getBook);
 router.delete("/:id", auth, bookCtrl.deleteBook);
-router.put("/:id", auth, multer, bookCtrl.modifyBook);
+router.put("/:id", auth, multer, imageCompression, bookCtrl.modifyBook);
 router.post("/:id/rating", auth, bookCtrl.addBookRating);
 
 module.exports = router;
