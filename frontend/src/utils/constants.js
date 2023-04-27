99d22011d889fc392d2bbf5b89bd8baa22ebe820
Rukia0124 <asahari02@gmail.com>
2023-04-27 21:49:44 +0200

    refactor: modified API_URL

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,5 @@
-const API_URL = process.env.API_URL || 'http://localhost:4000';
+const API_URL = 'http://mon-vieux-grimoire.rukia.fr:4000';
+
 export const API_ROUTES = {
   SIGN_UP: `${API_URL}/api/auth/signup`,
   SIGN_IN: `${API_URL}/api/auth/login`,
